ab4ca888be9f2c1be14956573bbaf1a29d3439aa
samuel quintero <s.quinteroc91@gmail.com>
2018-04-09 20:06:47 +0100

    Db Room small refactor

--- app/src/main/java/com/squintero/medicinapp/data/model/PersonData.java
+++ app/src/main/java/com/squintero/medicinapp/data/model/PersonData.java
@@ -4,7 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "persons")
 public class PersonData {
     @PrimaryKey
     private int id              = -1;
